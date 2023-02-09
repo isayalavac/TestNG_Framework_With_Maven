51a440b9ff66e28a449fb3cdbb3d2a0408f9da41
isayalavac <isayalavac@yahoo.com>
2023-02-09 13:45:03 -0500

    hw2

--- src/test/java/com/neotech/pages/DashboardPageElements.java
+++ src/test/java/com/neotech/pages/DashboardPageElements.java
@@ -8,8 +8,62 @@ import com.neotech.utils.CommonMethods;
 
 public class DashboardPageElements extends CommonMethods{
 	
+	@FindBy(xpath="//li[@id='menu_pim_viewPimModule']/a/span[2]")
+	public WebElement PIM;
+	
+	@FindBy(linkText="Add Employee")
+	public WebElement addEmployeeBtn;
+	
+	@FindBy(xpath="//div[@id='modal-holder']/div/div/div")
+	public WebElement employeeInfoScreen;
+	
+			@FindBy(id="first-name-box")
+			public WebElement firstNameBox;
+			
+			@FindBy(id="last-name-box")
+			public WebElement lastNameBox;
+			
+			@FindBy(id="employeeId")
+			public WebElement employeeId;
+			
+			@FindBy(id="location")
+			public WebElement locationDD;
+			
+			@FindBy(xpath="//label[@for='hasLoginDetails']")
+			public WebElement hasLoginDetails;
+					
+				   @FindBy(id="username")
+				   public WebElement username;
+				   
+				   @FindBy(id="status_Enabled")
+				   public WebElement statusEnabled;
+				   
+				   @FindBy(id="password")
+				   public WebElement password;
+				   
+				   @FindBy(id="confirmPassword")
+				   public WebElement confirmPassword;			   
+				   
+				   @FindBy(xpath="//div[contains(text(),'Default ESS')]")
+				   public WebElement essRoleId;	
+				   
+				   @FindBy(xpath="//div[contains(text(),'Default Supervisor')]")
+				   public WebElement defaultSupervisor;
+				   
+				   @FindBy(xpath="//select[@id='adminRoleId']")
+				   public WebElement adminRoleId;
+				   
+				   @FindBy(xpath="//label[@id='photo-preview-lable']")
+				   public WebElement employeePicture;
+				   
+				   @FindBy(id="modal-save-button")
+				   public WebElement saveButton;
+				   
+			
+	
 	@FindBy(id="account-name")
 	public WebElement accountName;
+	
 
 	
 	
--- src/test/java/com/neotech/testcases/AddEmployeeTest.java
+++ src/test/java/com/neotech/testcases/AddEmployeeTest.java
@@ -1,27 +1,68 @@
 package com.neotech.testcases;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.neotech.pages.DashboardPageElements;
 import com.neotech.pages.LoginPageElements;
 import com.neotech.utils.CommonMethods;
 import com.neotech.utils.ConfigsReader;
+import com.neotech.utils.ExcelUtility;
 
 public class AddEmployeeTest extends CommonMethods{
 	
-	@Test
-	public static void addEmployee() {
+	@Test(dataProvider = "excelData", groups = "addEmployees")
+	public static void addEmployee(String firstName, String lastname, String location, String employeeId, String loginUsername,
+								   String loginPassword, String adminRoleId ) {
+		
+//		Create AddEmployeeTest.java similar to Homework1.java under com/neotech/lesson04 package in TestNG project.
+//		Follow Page Object Model design pattern, don't find any elements inside the test method!
+//
+//		Create a Test Method named addEmployee with the following steps:
+//		Login into the application
+//		Navigate to PIM and Add Employee
+//		Provide First Name and Last Name
+//		Create Login Details
+//		Provide User Name and Password
+//		Save the Employee
+//		Verify Employee has been added successfully
+//
+//		This test method should belong to addEmp group.
+//		By using @DataProvider, add 3 different employees using Excel.xlsx file.
+//
+//		Create an xml file named addEmp.xml similar to smoke.xml file and execute the xml file.
+
 		LoginPageElements login = new LoginPageElements();
 		
 		sendText(login.username, ConfigsReader.getProperty("username"));
 		sendText(login.password, ConfigsReader.getProperty("password"));
 		wait(1);
 		click(login.logInButton);
+		wait(2);
 		
 		DashboardPageElements dashboard = new DashboardPageElements(); 
+		click(dashboard.PIM);
+		waitForVisibility(dashboard.addEmployeeBtn);
+		click(dashboard.addEmployeeBtn);
+		wait(2);
+		sendText(dashboard.firstNameBox, firstName);
+		sendText(dashboard.lastNameBox, lastname);
+		sendText(dashboard.employeeId, employeeId);
+		selectDropDown(dashboard.locationDD, location);
+		click(dashboard.hasLoginDetails);
+		wait(1);
+		sendText(dashboard.username, loginUsername);
+		sendText(dashboard.password, loginPassword);
+		sendText(dashboard.confirmPassword, loginPassword);
+		selectDropDown(dashboard.adminRoleId, adminRoleId);
+		click(dashboard.saveButton);
+
 		
 	}
 	
-	
+	@DataProvider(name = "excelData")
+	public Object [][] getExcelData(){
+		return ExcelUtility.excelIntoArray(System.getProperty("user.dir") + "/testdata/Excel.xlsx", "Employee" );
+	}
 
 }
